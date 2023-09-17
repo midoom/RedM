cd0e016f6013a4d45348de2ab26b20b1f215bdf7
ZebdeBob <mid72@hotmail.fr>
2023-09-17 18:22:08 +0200

    free mapping

---
+++ Mapping/mediccamp/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'Custom Medic camps'
+version '1.0.0'
+this_is_a_map 'yes'
+lua54 'yes'
---
+++ Mapping/mexico_camps_01/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+game "rdr3"
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+lua54 "yes"
+this_is_a_map "yes"
+
+
+files {
+	"stream/*.ymap",
+}
+  
